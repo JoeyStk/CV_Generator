8d9fb951763ee5fe148fe0e2ca999dd9986f405d
tosta <tobias.stach@gmail.com>
2023-10-02 18:25:45 +0200

    personal info model added

--- cv_generator/main/models.py
+++ cv_generator/main/models.py
@@ -18,3 +18,17 @@ class WorkingExperience(models.Model):
     
     def __str__(self):
         return self.company_name + ' | ' + self.title
+
+
+class PersonalInformation(models.Model):
+    name = models.CharField(max_length=25, blank=False)
+    email = models.EmailField(blank=False)
+    website_links = models.URLField(blank=True)
+    profile_photo = models.ImageField(upload_to="images/", blank=False)
+    phone_number = models.PositiveIntegerField(blank=False)
+    zip_code = models.PositiveIntegerField(blank=False)
+    street = models.CharField(max_length=50, blank=False)
+    city = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return f'{self.name} | {self.email}'
