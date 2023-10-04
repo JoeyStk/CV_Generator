aef176108d0802747630df7e21513daa6a5e4f1a
JoeyStk <johannes.stock@yahoo.de>
2023-10-04 10:08:06 +0200

    Add skill model

--- cv_generator/main/admin.py
+++ cv_generator/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import WorkingExperience, PersonalInformation
+from .models import Skill, PersonalInformation, WorkingExperience
 
 # Register your models here.
 admin.site.register(WorkingExperience)
 admin.site.register(PersonalInformation)
+admin.site.register(Skill)
